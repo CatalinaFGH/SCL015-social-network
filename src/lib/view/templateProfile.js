7062d53bf83ded8a6bb89c409937acce6cc8c298
Saya Yaczms <marce.catalan.castelli@gmail.com>
2021-02-25 00:10:37 -0300

    agregamos el crear perfil para el primer ingreso del usuario verificado y completamos el menu desplegable del muro principal

---
+++ src/lib/view/templateProfile.js
@@ -0,0 +1,50 @@
+export const profile = () => {
+ const divProfile = document.createElement("DIV");
+ const viewProfile = `
+ <header class="headerProfile" id="header">
+ <div class= "profileTitle"><h3>Crear Perfil</h3></div>
+ </header>
+<main class="newProfileContent">
+ <img src="" alt="" class="newProfileImage" id="profilePic"><br>
+ <div class="profileContentDiv">
+ <h1 id="profileName" class="viewProfileName"></h1>
+ <p class="profileContent">
+     Instagram<br>
+     <span id="profileInstagram"></span><br><br>
+     Facebook<br>
+     <span id="profileFacebook"></span><br><br>
+     Intereses<br>
+     <span id="profileAboutMe"></span>
+ </p>
+ </div>
+ </main>
+ <div class="btnsProfileContainer">
+ <button class="profileButton"><a href="#/createProfile" class="link">EDITAR</a></button>  <button class="profileButton"><a href="#/wall" class="link">HOME</a></button>
+ </div>
+  <footer class="fixedFooter">
+  </footer>
+ `
+ divProfile.innerHTML = viewProfile;
+
+ const firestore = firebase.firestore();
+ let currentUserData = firebase.auth().currentUser;
+ const uid = currentUserData.uid;
+ 
+ 
+ firestore.collection('users').doc(uid).get().then(function(doc){
+     if (doc.exists) {
+         divProfile.querySelector("#profileName").innerHTML = doc.data().name;
+         divProfile.querySelector("#profileInstagram").innerHTML = doc.data().instagram;
+         divProfile.querySelector("#profileFacebook").innerHTML = doc.data().facebook;
+         divProfile.querySelector("#profileAboutMe").innerHTML = doc.data().aboutMe;
+         divProfile.querySelector("#profilePic").src = doc.data().photoURL;
+     } else {
+         console.log("No such document!");
+     }
+ }).catch(function(error) {
+     console.log("Error getting document:", error);
+ });
+ 
+
+ return divProfile;
+}
